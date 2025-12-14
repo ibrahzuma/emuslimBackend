acd9a83d09709e3babaffb90254ebdf019c458bb
unknown <ibrahimcadabra1@gmail.com>
2025-12-14 17:22:45 +0300

    Fix import os syntax error

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import os
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 
